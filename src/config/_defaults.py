e5edd72728664578844603077e1d41d03f11259a
KlasterK <kadakinvi2@gmail.com>
2025-08-20 22:04:51 +0900

    Remake temp exchanging system

--- src/config/_defaults.py
+++ src/config/_defaults.py
@@ -16,55 +16,66 @@ class ConfigDefaults:
     MUSIC_VOLUME: float = 0
     DRAWING_IS_CIRCULAR: bool = True
     DRAWING_IS_DESTRUCTIVE: bool = False
-    WINDOW_CAPTION: str = 'moonsbox'
+    WINDOW_CAPTION: str = "moonsbox"
     SCREEN_SIZE: tuple = (560, 560)
     MAP_SIZE: tuple = (100, 100)
-    VISIBLE_AREA: pygame.Rect = field(default_factory=lambda: pygame.Rect(-20, -20, 140, 140))
+    VISIBLE_AREA: pygame.Rect = field(
+        default_factory=lambda: pygame.Rect(-20, -20, 140, 140)
+    )
     PALETTE_ICON_SIZE: tuple = (64, 64)
     PALETTE_MAX_CAPTION_CHARS: int = 20
     PALETTE_MARGIN: int = 20
-    MAP_INNER_COLOR: pygame.Color = field(default_factory=lambda: pygame.Color('#000000'))
-    MAP_OUTER_COLOR: pygame.Color = field(default_factory=lambda: pygame.Color('#071b10'))
+    MAP_INNER_COLOR: pygame.Color = field(
+        default_factory=lambda: pygame.Color("#000000")
+    )
+    MAP_OUTER_COLOR: pygame.Color = field(
+        default_factory=lambda: pygame.Color("#071b10")
+    )
     PALETTE_SELECTION_OUTER_COLOR: pygame.Color = field(
-        default_factory=lambda: pygame.Color('#00C3FF40')
+        default_factory=lambda: pygame.Color("#00C3FF40")
     )
     PALETTE_SELECTION_INNER_COLOR: pygame.Color = field(
-        default_factory=lambda: pygame.Color('#008CFF20')
+        default_factory=lambda: pygame.Color("#008CFF20")
     )
-    PALETTE_SHADOW_COLOR: pygame.Color = field(default_factory=lambda: pygame.Color('#000000CC'))
+    PALETTE_SHADOW_COLOR: pygame.Color = field(
+        default_factory=lambda: pygame.Color("#000000CC")
+    )
     FONT_SIZE: int = 15
-    FONT_NAME_OR_PATH: str = 'Sans'
+    FONT_NAME_OR_PATH: str = "Sans"
     FONT_IS_SYSFONT: bool = True
     ENABLE_VSYNC: bool = True
     ENABLE_FPS_COUNTER: bool = False
     ENABLE_TPS_COUNTER: bool = False
-    DEBUG_COLOR: pygame.Color = field(default_factory=lambda: pygame.Color('#00FF00'))
+    DEBUG_COLOR: pygame.Color = field(default_factory=lambda: pygame.Color("#00FF00"))
     USER_LOCALE: str = field(default_factory=lambda: locale.getlocale()[0])
     MATERIAL_TRANSLATIONS: dict = field(
         default_factory=lambda: {
-            'ru_RU': {
-                'Space': 'Стереть',
-                'Sand': 'Песок',
-                'Water': 'Вода',
-                'Unbreakable Wall': 'Неруш. Стена',
-                'Lava': 'Лава',
-                '+100 K': '+100 °C',
-                '-100 K': '-100 °C',
-                'Black Hole': 'Чёрная Дыра',
-                'Tap': 'Кран',
-                'Propane': 'Пропан',
-                'Fire': 'Огонь',
+            "ru_RU": {
+                "Space": "Стереть",
+                "Sand": "Песок",
+                "Water": "Вода",
+                "Unbreakable Wall": "Неруш. Стена",
+                "Lava": "Лава",
+                "+100 K": "+100 °C",
+                "-100 K": "-100 °C",
+                "Black Hole": "Чёрная Дыра",
+                "Tap": "Кран",
+                "Propane": "Пропан",
+                "Fire": "Огонь",
             },
-            'en_US': {
-                '+100 K': '+180 °F',
-                '-100 K': '-180 °F',
+            "en_US": {
+                "+100 K": "+180 °F",
+                "-100 K": "-180 °F",
             },
         }
     )
-    ASSETS_ROOT: Path = Path('./assets')
-    SCREENSHOT_PATH_FACTORY: callable = lambda: time.strftime('./screenshot_%Y-%m-%d_%H-%M-%S.png')
-    SCREENSHOT_TYPE_HINT: str = 'PNG'
+    ASSETS_ROOT: Path = Path("./assets")
+    SCREENSHOT_PATH_FACTORY: callable = lambda: time.strftime(
+        "./screenshot_%Y-%m-%d_%H-%M-%S.png"
+    )
+    SCREENSHOT_TYPE_HINT: str = "PNG"
     SCREENSHOT_DOT_SIZE: tuple = (1, 1)
-    CAPTURE_PATH_FACTORY: callable = lambda: time.strftime('./capture_%Y-%m-%d_%H-%M-%S/')
+    CAPTURE_PATH_FACTORY: callable = lambda: time.strftime(
+        "./capture_%Y-%m-%d_%H-%M-%S/"
+    )
     PALETTE_MOUSE_SELECTION_REQUIRES_DBL_CLICK: bool = True
-    DO_USE_FAST_TEMP_EXCHANGING: bool = True
