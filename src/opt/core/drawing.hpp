8b1e70004252ae2ac02bf1aec7808003655e637e
KlasterK <kadakinvi2@gmail.com>
2025-09-06 00:53:54 +0900

    Draft 2

--- src/opt/core/drawing.hpp
+++ src/opt/core/drawing.hpp
@@ -1,6 +1,23 @@
 #ifndef KK_OPT_DRAWING_HPP
 #define KK_OPT_DRAWING_HPP
 
-#include "gamemap.hpp"
+#include <modapi.h>
+#include "util.hpp"
+
+class GameMap;
+
+namespace GameMapDrawing
+{
+    enum class LineEnds { Square, Round };
+    using namespace LineEnds;
+
+    void fill(GameMap& map, setup_dot_func_t factory);
+
+    void draw_rect(GameMap& map, const Rect& rect, setup_dot_func_t factory);
+
+    void draw_ellipse(GameMap& map, const Rect& rect, setup_dot_func_t factory);
+
+    void 
+}
 
 #endif // include guard
