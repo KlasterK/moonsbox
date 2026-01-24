0d9c75b2067ac4990dbf93cb4c3dcfdf6b8960c2
KlasterK <kadakinvi2@gmail.com>
2026-01-24 23:32:14 +0900

    fix: remake MtlTag as a namespace with constexpr indices and add phys. b-h. revision number

--- libopt/materialdefs.hpp
+++ libopt/materialdefs.hpp
@@ -5,17 +5,23 @@
 #include <any>
 #include <cstdint>
 
-enum class MtlTag
+namespace MtlTag
 {
-    Solid, Bulk, Liquid, Gas, Space, Float, N
-};
-constexpr auto MtlTagsCount = static_cast<size_t>(MtlTag::N);
-using MaterialTags = std::bitset<MtlTagsCount>;
+    constexpr size_t Solid  = 0;
+    constexpr size_t Bulk   = 1;
+    constexpr size_t Liquid = 2;
+    constexpr size_t Gas    = 3;
+    constexpr size_t Space  = 4;
+    constexpr size_t Float  = 5;
+    constexpr size_t N      = 6;
+}
+using MaterialTags = std::bitset<MtlTag::N>;
 
 enum class MaterialPhysicalBehavior
 {
     Null, Sand, Liquid, LightGas, HeavyGas
 };
+constexpr auto MaterialPhysicalBehaviorRevision = 20260124ULL;
 
 using MaterialID = uintptr_t;
 
@@ -38,5 +44,18 @@ struct ConstDotProxy
     const std::any &aux;
     const MaterialID &id;
 };
+
+namespace MtlTag
+{
+    constexpr bool IsSparseness(const MaterialTags &tags) 
+        { return tags.test(MtlTag::Gas) || tags.test(MtlTag::Space); }
+
+    constexpr bool IsFlowable(const MaterialTags &tags) 
+        { return IsSparseness(tags) || tags.test(MtlTag::Liquid); }
+
+    constexpr bool IsMovable(const MaterialTags &tags) 
+        { return tags.test(MtlTag::Bulk) || tags.test(MtlTag::Liquid)
+              || tags.test(MtlTag::Gas)  || tags.test(MtlTag::Float); }
+}
 
 #endif // MOOX_MATERIALDEFS_HPP
