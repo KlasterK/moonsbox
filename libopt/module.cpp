a50ed493f7f5ff7250a6d3b7f000e76cdcbbbc83
KlasterK <kadakinvi2@gmail.com>
2026-01-25 14:02:25 +0900

    feat: implement compatible Python binding

--- libopt/module.cpp
+++ libopt/module.cpp
@@ -1,61 +1,182 @@
 #include <Python.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "gamemap.hpp"
 #include "drawing.hpp"
+#include "simulation.hpp"
+#include "materials.hpp"
+#include <optional>
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(opt, m)
+struct DotProxy
 {
-    auto pygame = py::module_::import("pygame");
+    GameMap &map;
+    size_t x, y;
+};
 
+void _assign_dot(GameMap &map, DotProxy proxy, int x, int y)
+{
+    map.temps(x, y) = proxy.map.temps(proxy.x, proxy.y);
+    map.heat_capacities(x, y) = proxy.map.heat_capacities(proxy.x, proxy.y);
+    map.thermal_conductivities(x, y) = proxy.map.thermal_conductivities(proxy.x, proxy.y);
+    map.colors(x, y) = proxy.map.colors(proxy.x, proxy.y);
+    map.tags(x, y) = proxy.map.tags(proxy.x, proxy.y);
+    map.physical_behaviors(x, y) = proxy.map.physical_behaviors(proxy.x, proxy.y);
+    map.auxs(x, y) = proxy.map.auxs(proxy.x, proxy.y);
+    map.material_ids(x, y) = proxy.map.material_ids(proxy.x, proxy.y);
+}
+
+Space g_space;
+Sand  g_sand;
+
+SimulationManager _make_simulation_manager(GameMap &map)
+{
+    SimulationManager sim(map);
+    sim.register_controller(g_space, "Space");
+    sim.register_controller(g_sand, "Sand");
+    return sim;
+}
+
+PYBIND11_MODULE(libopt, m)
+{
     py::class_<DotProxy>(m, "DotProxy")
         .def_property("temp",
-                      [](DotProxy &self) { return self.temp; },
-                      [](DotProxy &self, float value) { self.temp = value; })
+                      [](DotProxy self) 
+                      { return self.map.temps(self.x, self.y); },
+                      [](DotProxy self, float value) 
+                      { self.map.temps(self.x, self.y) = value; })
         .def_property("heat_capacity",
-                      [](DotProxy &self) { return self.heat_capacity; },
-                      [](DotProxy &self, float value) { self.heat_capacity = value; })
+                      [](DotProxy self) 
+                      { return self.map.heat_capacities(self.x, self.y); },
+                      [](DotProxy self, float value) 
+                      { self.map.heat_capacities(self.x, self.y) = value; })
         .def_property("thermal_conductivity",
-                      [](DotProxy &self) { return self.thermal_conductivity; },
-                      [](DotProxy &self, float value) { self.thermal_conductivity = value; })
+                      [](DotProxy self) 
+                      { return self.map.thermal_conductivities(self.x, self.y); },
+                      [](DotProxy self, float value) 
+                      { self.map.thermal_conductivities(self.x, self.y) = value; })
         .def_property("color",
-                      [](DotProxy &self) { return self.color; },
-                      [](DotProxy &self, uint32_t value) { self.color = value; })
+                      [](DotProxy self) 
+                      { static auto pygame_color = py::module_::import("pygame").attr("Color");
+                        return pygame_color(self.map.colors(self.x, self.y)); },
+                      [](DotProxy self, py::object value) 
+                      { self.map.colors(self.x, self.y) = py::int_(value); })
         .def_property("tags",
-                      [](DotProxy &self) { return self.tags.to_ullong(); },
-                      [](DotProxy &self, unsigned long long value) { self.tags = value; })
+                      [](DotProxy self) 
+                      { return self.map.tags(self.x, self.y).to_ullong(); },
+                      [](DotProxy self, unsigned long long value) 
+                      { self.map.tags(self.x, self.y) = value; })
         .def_property("physical_entity",
-                      [](DotProxy &self) { return static_cast<unsigned long long>(self.physical_behavior); },
-                      [](DotProxy &self, unsigned long long value) { self.physical_behavior = static_cast<MaterialPhysicalBehavior>(value); })
+                      [](DotProxy self) 
+                      { return static_cast<unsigned long long>(
+                        self.map.physical_behaviors(self.x, self.y)); },
+                      [](DotProxy self, unsigned long long value) 
+                      { self.map.physical_behaviors(self.x, self.y) 
+                        = static_cast<MaterialPhysicalBehavior>(value); })
     ;
 
     py::class_<GameMap>(m, "GameMap")
+        .def(py::init([](std::array<size_t, 2> size) 
+        { 
+            GameMap map(size[0], size[1]);
+            drawing::fill(map, [&](GameMap &map, size_t x, size_t y)
+                               { g_space.init_point(map, x, y); });
+            return map;
+        }))
+        .def_property_readonly("size", [](GameMap &map) 
+        {
+            return py::make_tuple(map.width(), map.height());
+        })
         .def("__getitem__", [](GameMap &map, std::array<int, 2> pos) -> py::object
         {
             if(!map.in_bounds(pos[0], pos[1]))
                 return py::none();
-            return py::cast(map.make_proxy(pos[0], pos[1]), py::return_value_policy::reference_internal);
+            return py::cast(DotProxy{map, (size_t)pos[0], (size_t)pos[1]});
         })
-        .def("__setitem__", [](GameMap &map, std::array<int, 2> pos, DotProxy value)
+        .def("__setitem__", [](GameMap &map, std::array<int, 2> pos, DotProxy proxy)
         {
             if(!map.in_bounds(pos[0], pos[1]))
                 return;
-            map.temps(pos[0], pos[1]) = value.temp;
-            map.heat_capacities(pos[0], pos[1]) = value.heat_capacity;
-            map.thermal_conductivities(pos[0], pos[1]) = value.thermal_conductivity;
-            map.colors(pos[0], pos[1]) = value.color;
-            map.tags(pos[0], pos[1]) = value.tags;
-            map.physical_behaviors(pos[0], pos[1]) = value.physical_behavior;
-            map.auxs(pos[0], pos[1]) = value.aux;
-            map.material_ids(pos[0], pos[1]) = value.id;
+
+            _assign_dot(map, proxy, pos[0], pos[1]);
         })
         .def("invy", [](GameMap &map, int y) { return map.height() - 1 - y; })
         .def("invy_pos", [](GameMap &map, std::array<int, 2> pos)
         {
             return std::array{pos[0], int(map.height() - 1 - pos[1])};
         })
-        .def("bounds", [](GameMap &map, std::array<int, 2> pos) { return map.in_bounds(pos[0], pos[1]); })
-        .def("fill", &drawing::fill)
+        .def("bounds", [](GameMap &map, std::array<int, 2> pos) 
+        { 
+            return map.in_bounds(pos[0], pos[1]); 
+        })
+        .def("fill", [](GameMap &map, py::function material_factory) 
+        {
+            drawing::fill(map, [&](GameMap &, size_t x, size_t y)
+            {
+                auto obj = material_factory(map, x, y);
+                _assign_dot(map, py::cast<DotProxy>(obj), x, y);
+            });
+        })
+        .def("draw_rect", [](GameMap &map, drawing::Rect area, py::function material_factory) 
+        {
+            drawing::rect(map, area, [&](GameMap &, size_t x, size_t y)
+            {
+                auto obj = material_factory(map, x, y);
+                _assign_dot(map, py::cast<DotProxy>(obj), x, y);
+            });
+        })
+        .def("draw_ellipse", [](GameMap &map, drawing::Rect area, py::function material_factory) 
+        {
+            drawing::ellipse(map, area, [&](GameMap &, size_t x, size_t y)
+            {
+                auto obj = material_factory(map, x, y);
+                _assign_dot(map, py::cast<DotProxy>(obj), x, y);
+            });
+        })
+        .def("draw_line", [](GameMap &map, std::array<int, 2> begin, std::array<int, 2> end, 
+                             int width, py::function material_factory, std::string_view ends) 
+        {
+            drawing::LineEnds ends_value;
+            if(ends == "square")
+                ends_value = drawing::LineEnds::Square;
+            else if(ends == "round")
+                ends_value = drawing::LineEnds::Round;
+            else
+                throw py::value_error(std::format("Invalid line ends '{}'", ends));
+
+            drawing::line(map, begin, end, width, [&](GameMap &, size_t x, size_t y)
+            {
+                auto obj = material_factory(map, x, y);
+                _assign_dot(map, py::cast<DotProxy>(obj), x, y);
+            }, ends_value);
+        })
+        .def("dump", [](GameMap &map, py::object) {})
+        .def("load", [](GameMap &map, py::object) {})
     ;
+
+    py::class_<SimulationManager>(m, "SimulationManager")
+        .def(py::init([](GameMap &map) { return _make_simulation_manager(map); }))
+        .def("tick", [](SimulationManager &sim) { sim.tick(); })
+        .def("tick", [](SimulationManager &sim, py::object) { sim.tick(); })
+        .def("get_tps", []{ return 0.0; })
+    ;
+
+    py::class_<MaterialController>(m, "MaterialController")
+        .def("__call__", [](MaterialController &ctl, GameMap &map, size_t x, size_t y)
+        { 
+            ctl.init_point(map, x, y);
+            return DotProxy(map, x, y);
+        })
+    ;
+
+    m.def("ls_materials", []
+    {
+        py::dict d;
+        d["Space"] = py::cast(static_cast<MaterialController *>(&g_space), 
+                              py::return_value_policy::reference);
+        d["Sand"]  = py::cast(static_cast<MaterialController *>(&g_sand), 
+                              py::return_value_policy::reference);
+        return d;
+    });
 }
