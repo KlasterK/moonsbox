19593411d14d1975ae6e7d514701232989dfe366
KlasterK <kadakinvi2@gmail.com>
2026-01-25 13:52:23 +0900

    feat: add procedures for drawing ellipses and lines (4-connected, widthed)

--- libopt/drawing.hpp
+++ libopt/drawing.hpp
@@ -84,4 +84,94 @@ void drawing::rect(GameMap &map, Rect area, T material_factory)
     }
 }
 
+template<typename T>
+void drawing::ellipse(GameMap &map, Rect area, T material_factory)
+{
+    // Solving ellipse equation:
+    // ((x - x0) / a) ** 2 + ((y - y0) / b) ** 2 <= 1
+    // This formula with only integral logic:
+    // b ** 2 * (x - x0) ** 2 + a ** 2 * (y - y0) ** 2 <= a ** 2 * b ** 2
+
+    long long x0, y0, a_sq, b_sq, right_cmp, rows = map.height(), cols = map.width();
+    x0 = area[0] + area[2] / 2;
+    y0 = area[1] + area[3] / 2;
+    a_sq = area[2] * area[2] / 4;
+    b_sq = area[3] * area[3] / 4;
+    right_cmp = a_sq * b_sq;
+
+    for (long long y = 0; y < rows; ++y) 
+    {
+        for (long long x = 0; x < cols; ++x) 
+        {
+            long long x_x0_sq = (x - x0) * (x - x0);
+            long long y_y0_sq = (y - y0) * (y - y0);
+            long long left_cmp = b_sq * x_x0_sq + a_sq * y_y0_sq;
+            if(left_cmp < right_cmp) 
+                material_factory(map, x, y);
+        }
+    }
+}
+
+// TODO: implement more efficient line drawing (this one is just translation from Python)
+template<typename T>
+void drawing::line(GameMap &map, SignedPoint begin, SignedPoint end, 
+                   int width, T material_factory, LineEnds ends)
+{
+    int delta_x = std::abs(begin[0] - end[0]);
+    int delta_y = std::abs(begin[1] - end[1]);
+
+    int current_x = begin[0];
+    int current_y = begin[1];
+
+    int step_x = begin[0] < end[0] ? 1 : -1;
+    int step_y = begin[1] < end[1] ? 1 : -1;
+
+    int error{};
+    int radius = std::max(0, width / 2);
+
+    // 4-connected line algorithm
+    for(size_t i{}; i < delta_x + delta_y; ++i)
+    {
+        int e1 = error + delta_y;
+        int e2 = error - delta_x;
+        if(std::abs(e1) < std::abs(e2))
+        {
+            current_x += step_x;
+            error = e1;
+        }
+        else
+        {
+            current_y += step_y;
+            error = e2;
+        }
+
+        for(int radius_delta_x = -radius; radius_delta_x <= radius; ++radius_delta_x)
+        {
+            for(int radius_delta_y = -radius; radius_delta_y <= radius; ++radius_delta_y)
+            {
+                int tx = current_x + radius_delta_x;
+                int ty = current_y + radius_delta_y;
+                if(!map.in_bounds(tx, ty))
+                    continue;
+                if(ends == LineEnds::Round 
+                        && (radius_delta_x * radius_delta_x + radius_delta_y * radius_delta_y) 
+                        > radius * radius)
+                    continue;
+                material_factory(map, tx, ty);
+            }
+        }
+    }
+
+    //     for rad_dx in range(-radius, radius + 1):
+    //         for rad_dy in range(-radius, radius + 1):
+    //             tx, ty = x + rad_dx, y + rad_dy
+    //             if not self.bounds((tx, ty)):
+    //                 continue
+    //             if ends == 'round':
+    //                 # Only fill points inside the circle
+    //                 if rad_dx**2 + rad_dy**2 > radius**2:
+    //                     continue
+    //             self._array[tx, ty] = material_factory(self, tx, ty)
+}
+
 #endif // DRAWING_HPP
