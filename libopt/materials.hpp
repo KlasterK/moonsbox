f5af6257817b37e7298f3e3649cd6c2281f65b0b
KlasterK <kadakinvi2@gmail.com>
2026-02-01 02:20:10 +0900

    feat: Aerogel and Dry Ice material

--- libopt/materials.hpp
+++ libopt/materials.hpp
@@ -839,4 +839,103 @@ private:
 };
 
 
+class Aerogel : public MaterialController
+{
+public:
+    inline void init_point(GameMap &map, size_t x, size_t y) override
+    {
+        map.temps(x, y) = 300.f;
+        map.heat_capacities(x, y) = 0.99f;
+        map.thermal_conductivities(x, y) = 0.01f;
+        uint32_t grayscale = 0xAA + rand() % 0x12;
+        map.colors(x, y) = grayscale << 24 | grayscale << 16 | grayscale << 8 | 0x25;
+        map.tags(x, y).reset().set(MtlTag::Solid);
+        map.auxs(x, y).reset();
+        map.physical_behaviors(x, y) = MaterialPhysicalBehavior::Null;
+        map.material_ids(x, y) = material_id();
+    }
+};
+
+
+class DryIce : public MaterialController
+{
+public:
+    inline void init_point(GameMap &map, size_t x, size_t y) override
+    {
+        int random_value = rand();
+        map.colors(x, y) = (
+            _map_clamp(random_value, 0, RAND_MAX, 0xDB, 0xC2) << 24
+            | _map_clamp(random_value, 0, RAND_MAX, 0xE2, 0xD9) << 16
+            | _map_clamp(random_value, 0, RAND_MAX, 0xEE, 0xDF) << 8
+        );
+        map.temps(x, y) = 175.f;
+        map.heat_capacities(x, y) = 0.95f;
+        map.thermal_conductivities(x, y) = 1.f;
+        map.tags(x, y).reset().set(MtlTag::Bulk);
+        map.auxs(x, y).reset();
+        map.physical_behaviors(x, y) = MaterialPhysicalBehavior::Sand;
+        map.material_ids(x, y) = material_id();
+    }
+
+    inline void static_update(GameMap &map) override
+    {
+        if(m_sim == nullptr)
+            throw std::logic_error("Absorbent was never registered in SimulationManager");
+
+        if(m_space == nullptr)
+        {
+            m_space = m_sim->find_controller_by_name("Space");
+            if(m_space == nullptr)
+                throw std::logic_error(
+                    "Absorbent cannot not find Space material in SimulationManager"
+                );
+        }
+
+        for(size_t y{}; y < map.height(); ++y)
+        {
+            for(size_t x{}; x < map.width(); ++x)
+            {
+                if(map.material_ids(x, y) != this->material_id())
+                    continue;
+
+                auto temp = static_cast<int32_t>(map.temps(x, y));
+                if(temp > 250)
+                {
+                    m_space->init_point(map, x, y);
+                }
+                else if(temp > 195)
+                {
+                    map.tags(x, y).reset().set(MtlTag::Gas);
+                    map.physical_behaviors(x, y) = MaterialPhysicalBehavior::HeavyGas;
+                }
+                else
+                {
+                    map.tags(x, y).reset().set(MtlTag::Bulk);
+                    map.physical_behaviors(x, y) = MaterialPhysicalBehavior::Sand;
+                }
+
+                uint32_t &color = map.colors(x, y);
+                color &= 0xFFFFFF00;
+                color |= _map_clamp(temp, 175, 250, 0xFF, 0x00);
+            }
+        }
+    }
+
+    inline bool is_placeable_on(GameMap &map, size_t x, size_t y) override
+    {
+        auto &tags = map.tags(x, y);
+        return MtlTag::IsMovable(tags) || tags.test(MtlTag::Space);
+    }
+
+    inline void on_register(SimulationManager &sim) override
+    {
+        m_sim = &sim;
+    }
+
+private:
+    SimulationManager *m_sim = nullptr;
+    MaterialController *m_space = nullptr;
+};
+
+
 #endif // MOOX_MATERIALS_HPP
--- libopt/module.cpp
+++ libopt/module.cpp
@@ -39,7 +39,7 @@ void _assign_dot(GameMap &map, DotProxy proxy, int x, int y)
 std::tuple<
     Space, Sand, Plus100K, Minus100K, Water, Ice, Steam,
     Tap, UnbreakableWall, BlackHole, Propane, Fire, PureGlass,
-    Lava, Absorbent
+    Lava, Absorbent, Aerogel, DryIce
 > g_materials_tuple{};
 
 auto g_materials_map = std::to_array<std::pair<std::string_view, MaterialController &>>({
@@ -58,6 +58,8 @@ auto g_materials_map = std::to_array<std::pair<std::string_view, MaterialControl
     {"Glass",               std::get<PureGlass>(g_materials_tuple)},
     {"Lava",                std::get<Lava>(g_materials_tuple)},
     {"Absorbent",           std::get<Absorbent>(g_materials_tuple)},
+    {"Aerogel",             std::get<Aerogel>(g_materials_tuple)},
+    {"Dry Ice",             std::get<DryIce>(g_materials_tuple)},
 });
 
 std::unique_ptr<SimulationManager> _make_simulation_manager(GameMap &map)
