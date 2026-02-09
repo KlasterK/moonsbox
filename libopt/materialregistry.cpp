bc0c25cf6effd9e4203b9a403c8dc333a10afc79
KlasterK <kadakinvi2@gmail.com>
2026-02-09 17:28:22 +0900

    chore: add get_name_by_id method to MaterialRegistry

--- libopt/materialregistry.cpp
+++ libopt/materialregistry.cpp
@@ -1,19 +1,27 @@
 #include "materialregistry.hpp"
-
+#include <optional>
 
 bool MaterialRegistry::register_controller(MaterialController &controller, std::string_view name)
 {
-    auto [_, was_inserted] = m_controllers.insert({name, &controller});
-    if(was_inserted)
-        controller.on_register(*this);
-    return was_inserted;
-}
+    auto [name_ctl_it, name_ctl_was_inserted] = m_name_ctl_map.insert({name, &controller});
+    if(!name_ctl_was_inserted)
+        return false;
 
+    auto [_, ctl_name_was_inserted] = m_ctl_name_map.insert({&controller, name});
+    if(!ctl_name_was_inserted)
+    {
+        m_name_ctl_map.erase(name_ctl_it);
+        return false;
+    }
+
+    controller.on_register(*this);
+    return true;
+}
 
 MaterialController *MaterialRegistry::find_controller_by_name(std::string_view name) const
 {
-    auto it = m_controllers.find(name);
-    if(it == m_controllers.end())
+    auto it = m_name_ctl_map.find(name);
+    if(it == m_name_ctl_map.end())
         return nullptr;
     
     return it->second;
@@ -21,11 +29,18 @@ MaterialController *MaterialRegistry::find_controller_by_name(std::string_view n
 
 MaterialController *MaterialRegistry::find_controller_by_id(MaterialID id) const
 {
-    auto id_as_ptr = reinterpret_cast<MaterialController *>(id);
-    for(auto &[_, ctl] : m_controllers)
-    {
-        if(ctl == id_as_ptr)
-            return ctl;
-    }
-    return nullptr;
+    auto it = m_ctl_name_map.find(reinterpret_cast<MaterialController *>(id));
+    if(it == m_ctl_name_map.end())
+        return nullptr;
+    
+    return it->first;
+}
+
+std::optional<std::string_view> MaterialRegistry::get_name_by_id(MaterialID id) const
+{
+    auto it = m_ctl_name_map.find(reinterpret_cast<MaterialController *>(id));
+    if(it == m_ctl_name_map.end())
+        return std::nullopt;
+    
+    return it->second;
 }
--- libopt/materialregistry.hpp
+++ libopt/materialregistry.hpp
@@ -2,6 +2,7 @@
 #define MOOX_MATERIALREGISTRY_HPP
 
 #include "materialcontroller.hpp"
+#include <string_view>
 #include <unordered_map>
 
 class MaterialRegistry
@@ -10,12 +11,14 @@ public:
     bool register_controller(MaterialController &controller, std::string_view name);
     MaterialController *find_controller_by_name(std::string_view name) const;
     MaterialController *find_controller_by_id(MaterialID id) const;
+    std::optional<std::string_view> get_name_by_id(MaterialID id) const;
 
-    inline auto begin() const { return m_controllers.begin(); }
-    inline auto end() const { return m_controllers.end(); }
+    inline auto begin() const { return m_name_ctl_map.begin(); }
+    inline auto end() const { return m_name_ctl_map.end(); }
 
 private:
-    std::unordered_map<std::string_view, MaterialController *> m_controllers;
+    std::unordered_map<std::string_view, MaterialController *> m_name_ctl_map;
+    std::unordered_map<MaterialController *, std::string_view> m_ctl_name_map;
 };
 
 #endif // MOOX_MATERIALREGISTRY_HPP
