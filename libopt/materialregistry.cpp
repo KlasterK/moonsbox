d3e1b54edfe4032719f5080f716e9cbcc3c9e5cb
KlasterK <kadakinvi2@gmail.com>
2026-02-15 02:52:57 +0900

    refactor: make MaterialRegistry::begin/end() return contigious iterators and change internal containers so material registering order is saved

--- libopt/materialregistry.cpp
+++ libopt/materialregistry.cpp
@@ -3,42 +3,43 @@
 
 bool MaterialRegistry::register_controller(MaterialController &controller, std::string_view name)
 {
-    auto [name_ctl_it, name_ctl_was_inserted] = m_name_ctl_map.insert({name, &controller});
-    if(!name_ctl_was_inserted)
-        return false;
-
-    auto [_, ctl_name_was_inserted] = m_ctl_name_map.insert({&controller, name});
-    if(!ctl_name_was_inserted)
+    for(auto &[reg_name, reg_ctl] : m_materials_table)
     {
-        m_name_ctl_map.erase(name_ctl_it);
-        return false;
+        if(name == reg_name || &controller == reg_ctl)
+            return false;
     }
-
+    m_materials_table.emplace_back(name, &controller);
     controller.on_register(*this);
     return true;
 }
 
 MaterialController *MaterialRegistry::find_controller_by_name(std::string_view name) const
 {
-    auto it = m_name_ctl_map.find(name);
-    if(it == m_name_ctl_map.end())
-        return nullptr;
-    
-    return it->second;
+    for(auto &[reg_name, reg_ctl] : m_materials_table)
+    {
+        if(name == reg_name)
+            return reg_ctl;
+    }
+    return nullptr;
 }
 
 std::optional<std::string_view> 
     MaterialRegistry::get_name_of_controller(const MaterialController *ctl) const
 {
-    // We only store pointers so const cast is not a problem
-    auto it = m_ctl_name_map.find(const_cast<MaterialController *>(ctl));
-    if(it == m_ctl_name_map.end())
-        return std::nullopt;
-    
-    return it->second;
+    for(auto &[reg_name, reg_ctl] : m_materials_table)
+    {
+        if(ctl == reg_ctl)
+            return reg_name;
+    }
+    return std::nullopt;
 }
 
-bool MaterialRegistry::is_controller_registered(const MaterialController *ctl) const
+bool MaterialRegistry::is_controller_registered(const MaterialController &ctl) const
 {
-    return m_ctl_name_map.contains(const_cast<MaterialController *>(ctl));
+    for(auto &[reg_name, reg_ctl] : m_materials_table)
+    {
+        if(&ctl == reg_ctl)
+            return false;
+    }
+    return true;
 }
--- libopt/materialregistry.hpp
+++ libopt/materialregistry.hpp
@@ -3,7 +3,7 @@
 
 #include "materialcontroller.hpp"
 #include <string_view>
-#include <unordered_map>
+#include <vector>
 
 class MaterialRegistry
 {
@@ -11,14 +11,13 @@ public:
     bool register_controller(MaterialController &controller, std::string_view name);
     MaterialController *find_controller_by_name(std::string_view name) const;
     std::optional<std::string_view> get_name_of_controller(const MaterialController *ctl) const;
-    bool is_controller_registered(const MaterialController *ctl) const;
+    bool is_controller_registered(const MaterialController &ctl) const;
 
-    inline auto begin() const { return m_name_ctl_map.begin(); }
-    inline auto end() const { return m_name_ctl_map.end(); }
+    inline auto begin() const { return m_materials_table.begin(); }
+    inline auto end() const { return m_materials_table.end(); }
 
 private:
-    std::unordered_map<std::string_view, MaterialController *> m_name_ctl_map;
-    std::unordered_map<MaterialController *, std::string_view> m_ctl_name_map;
+    std::vector<std::pair<std::string, MaterialController *>> m_materials_table;
 };
 
 #endif // MOOX_MATERIALREGISTRY_HPP
