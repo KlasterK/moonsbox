e9b30b17ae59831d5dd914656beda54d3896ce87
KlasterK <kadakinvi2@gmail.com>
2026-03-21 02:21:47 +0900

    feat: make fully featured drawing for touchscreens keeping in-browser pinch zoom
    - Touchscreen support is fully complete.
    - Dragging with 2 fingers should lead to in-browser pinch zoom, drawing is disabled in this moment.
    - Dragging with 3+ fingers enables drawing back, though 3+ fingers usually are dedicated to system actions.
    - Web Sound API resuming improved: now we make ctx.resume() attempts without any stop.
    - Add a button which may help with making sounds play, on click it tries to resume the context and deletes.

--- webmoonsbox/brush.js
+++ webmoonsbox/brush.js
@@ -5,15 +5,22 @@ var Module;
 class Brush {
     constructor(canvas) {
         this.material = 'Sand';
-        this.isHolded = false;
         this.thickness = 1;
-        this.previousPos = {x: 0, y: 0};
         this.isCircular = true;
         this.canvas = canvas;
 
-        document.addEventListener('mousedown', e => this.onPressed(e.clientX, e.clientY));
-        document.addEventListener('mousemove', e => this.onMoved(e.clientX, e.clientY));
-        document.addEventListener('mouseup', () => this.onReleased());
+        this.mousePreviousPos = {x: 0, y: 0};
+        this.isMouseHolded = false;
+        this.activeTouches = new Map();
+
+        canvas.addEventListener('mousedown',   e => this.onMousePressed(e));
+        document.addEventListener('mousemove', e => this.onMouseMoved(e));
+        document.addEventListener('mouseup',  () => this.onMouseReleased());
+
+        canvas.addEventListener('touchstart',    e => this.onTouchStarted(e), {passive: true});
+        document.addEventListener('touchmove',   e => this.onTouchMoved(e),   {passive: true});
+        document.addEventListener('touchend',    e => this.onTouchStopped(e), {passive: true});
+        document.addEventListener('touchcancel', e => this.onTouchStopped(e), {passive: true});
     }
 
     mouseToMapCoords(clientX, clientY) {
@@ -30,26 +37,32 @@ class Brush {
         };
     }
             
-    onPressed(clientX, clientY) {
-        if(this.externOnActivationCB != null)
+    onMousePressed(e) {
+        if(this.externOnActivationCB != null) {
             this.externOnActivationCB();
+        }
 
-        this.isHolded = true;
-        this.previousPos = this.mouseToMapCoords(clientX, clientY);
+        this.isMouseHolded = true;
+        this.mousePreviousPos = this.mouseToMapCoords(e.clientX, e.clientY);
         this.updateDrawing();
     }
             
-    onMoved(clientX, clientY) {
-        if(!this.isHolded)
-            return;
+    onMouseMoved(e) {
+        if(this.externOnActivationCB != null) {
+            this.externOnActivationCB();
+        }
 
-        let point = this.mouseToMapCoords(clientX, clientY);
+        if(!this.isMouseHolded) {
+            return;
+        }
+
+        let point = this.mouseToMapCoords(e.clientX, e.clientY);
         Module.ccall(
             'draw_line', 'boolean', 
             ['number', 'number', 'number', 'number', 'number', 'string', 'number'], 
             [
-                this.previousPos.x,
-                this.previousPos.y,
+                this.mousePreviousPos.x,
+                this.mousePreviousPos.y,
                 point.x,
                 point.y,
                 this.thickness,
@@ -62,37 +75,109 @@ class Brush {
             ['number', 'number', 'string'], 
             [point.x, point.y, this.material]
         );
-        this.previousPos = point;
+        this.mousePreviousPos = point;
     }
 
-    onReleased() {
-        if(this.externOnActivationCB != null)
+    onMouseReleased() {
+        if(this.externOnActivationCB != null) {
             this.externOnActivationCB();
+        }
 
-        this.isHolded = false;
+        this.isMouseHolded = false;
+    }
+
+    onTouchStarted(e) {
+        if(this.externOnActivationCB != null) {
+            this.externOnActivationCB();
+        }
+
+        for(let touch of e.changedTouches) {
+            this.activeTouches.set(touch.identifier, {
+                previousPos: this.mouseToMapCoords(touch.clientX, touch.clientY),
+            });
+        }
+
+        this.updateDrawing();
+    }
+
+    onTouchMoved(e) {
+        if(this.externOnActivationCB != null) {
+            this.externOnActivationCB();
+        }
+
+        if(this.activeTouches.size > 1) {
+            return;
+        }
+
+        for(let touch of e.changedTouches) {
+            if(!this.activeTouches.has(touch.identifier)) {
+                continue;
+            }
+
+            let point = this.mouseToMapCoords(touch.clientX, touch.clientY);
+            Module.ccall(
+                'draw_line', 'boolean', 
+                ['number', 'number', 'number', 'number', 'number', 'string', 'number'], 
+                [
+                    this.activeTouches.get(touch.identifier).previousPos.x,
+                    this.activeTouches.get(touch.identifier).previousPos.y,
+                    point.x,
+                    point.y,
+                    this.thickness,
+                    this.material,
+                    0, // drawing::LineEnds::None
+                ],
+            );
+            Module.ccall(
+                'play_place_sound', 'boolean', 
+                ['number', 'number', 'string'], 
+                [point.x, point.y, this.material]
+            );
+            this.activeTouches.get(touch.identifier).previousPos = point;
+        }
+    }
+
+    onTouchStopped(e) {
+        if(this.externOnActivationCB != null) {
+            this.externOnActivationCB();
+        }
+
+        for (let touch of e.changedTouches) {
+            this.activeTouches.delete(touch.identifier);
+        }
     }
 
     updateDrawing() {
-        if(!this.isHolded)
-            return;
+        let points = [];
+        if(this.isMouseHolded) {
+            points.push(this.mousePreviousPos);
+        }
 
-        Module.ccall(
-            'draw_rect_or_ellipse', 'boolean', 
-            ['number', 'number', 'number', 'number', 'boolean', 'string'], 
-            [
-                this.previousPos.x - this.thickness / 2,
-                this.previousPos.y - this.thickness / 2,
-                this.thickness,
-                this.thickness,
-                this.isCircular,
-                this.material,
-            ],
-        );
-        Module.ccall(
-            'play_place_sound', 'boolean', 
-            ['number', 'number', 'string'], 
-            [this.previousPos.x, this.previousPos.y, this.material]
-        );
+        if(this.activeTouches.size !== 2) {
+            for(let touch of this.activeTouches.values()) {
+                points.push(touch.previousPos);
+            }
+        }
+
+        for(let point of points) {
+            Module.ccall(
+                'draw_rect_or_ellipse', 'boolean', 
+                ['number', 'number', 'number', 'number', 'boolean', 'string'], 
+                [
+                    point.x - this.thickness / 2,
+                    point.y - this.thickness / 2,
+                    this.thickness,
+                    this.thickness,
+                    this.isCircular,
+                    this.material,
+                ],
+            );
+            Module.ccall(
+                'play_place_sound', 'boolean', 
+                ['number', 'number', 'string'], 
+                [point.x, point.y, this.material]
+            );
+        }
     }
 
     setOnActivationCB(cb) {
