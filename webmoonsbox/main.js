10bf453249df55e87a8fa155e5f3ca1c137ab78b
KlasterK <kadakinvi2@gmail.com>
2026-03-26 20:20:01 +0900

    refactor: move code from index.html to main.js

---
+++ webmoonsbox/main.js
@@ -0,0 +1,177 @@
+'use strict';
+
+var defaultSize = {x: 240, y: 135};
+document.getElementById('brush-size-slider').value = 1;
+document.getElementById('reset-form-width').value  = defaultSize.x;
+document.getElementById('reset-form-height').value = defaultSize.y;
+document.getElementById('saving-file-input').value = '';
+
+var soundSystem = new SoundSystem();
+soundSystem.loadSounds();
+
+var canvas = document.getElementById('game-surface');
+canvas.style.height = Math.min(canvas.clientHeight, window.innerHeight * 0.5) + 'px';
+canvas.style.width = 'auto';
+
+var brush = new Brush(canvas);
+brush.setOnActivationCB(() => soundSystem.audioCtx.resume());
+
+var framesCount = 0;
+var buffers;
+var pixels;
+var brush;
+var gl;
+var rendererGL;
+var isInited = false;
+
+function initGame(size, doReinit = true) {
+    canvas.width = size.x;
+    canvas.height = size.y;
+    canvas.style.aspectRatio = `${size.x} / ${size.y}`;
+
+    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    if(gl == null) {
+        console.error('Cannot get WebGL context.');
+    }
+    gl.viewport(0, 0, size.x, size.y);
+    rendererGL = new RendererGL(gl, size.x, size.y);
+
+    buffers = JSON.parse(Module.UTF8ToString(Module._init_game(size.x, size.y, doReinit)));
+    pixels = new Uint8Array(wasmMemory.buffer, buffers.colors_ptr, size.x * size.y * 4);
+
+    initMaterialPalette(brush);
+    let playSoundCBID = Module.addFunction(soundSystem.playSoundCB.bind(soundSystem), 'vppi');
+    Module._register_play_sound_callback(playSoundCBID);
+
+    isInited = true;
+}
+
+function tickRender() {
+    if(isInited) {
+        Module._tick(true, false, false);
+        rendererGL.render(gl, pixels);
+        brush.updateDrawing();
+    }
+    ++framesCount;
+    requestAnimationFrame(tickRender);
+}
+tickRender();
+
+var Module = {
+    onRuntimeInitialized: function() {
+        initGame(defaultSize);
+    },
+};
+
+const pauseBtn = document.getElementById('pause-btn');
+
+function pauseGame() {
+    const v = Module._tick(false, false, false);
+    Module._tick(false, true, !v);
+
+    pauseBtn.textContent = v ? 'Pause' : 'Unpause';
+}
+
+const resetFormWidth = document.getElementById('reset-form-width');
+const resetFormHeight = document.getElementById('reset-form-height');
+
+function resetMap() {
+    const size = {
+        x: parseInt(resetFormWidth.value),
+        y: parseInt(resetFormHeight.value),
+    };
+
+    if(size.x == null || size.y == null) {
+        window.alert('adsa');
+        return;
+    }
+
+    initGame(size);
+}
+
+function formatSaveFileNameWithDate(date) {
+    const zfill2 = (n, width = 2) => String(n).padStart(width, '0');
+    
+    const year    = date.getFullYear();
+    const month   = zfill2(date.getMonth() + 1);
+    const day     = zfill2(date.getDate());
+    const hours   = zfill2(date.getHours());
+    const minutes = zfill2(date.getMinutes());
+    const seconds = zfill2(date.getSeconds());
+    
+    return `moonsbox ${year}-${month}-${day} ${hours}-${minutes}-${seconds}.kk-save`;
+}
+
+let lastSavingBlobURL = null;
+const savingLink = document.getElementById('saving-download-link');
+
+function saveGame() {
+    try {
+        FS.unlink('/out.kk-save');
+    } catch {}
+
+    const error = Module.ccall('minizip_load_store', 'string', ['boolean', 'string'], [true, '/out.kk-save']);
+    if(error.trim() != '') {
+        window.alert(`Error when saving your game! ${error}`);
+        return;
+    }
+
+    const zipData = FS.readFile('/out.kk-save');
+    const blob = new Blob([zipData], {type: 'application/x-moonsbox-save'});
+
+    if(lastSavingBlobURL != null) {
+        URL.revokeObjectURL(lastSavingBlobURL);
+    }
+    lastSavingBlobURL = URL.createObjectURL(blob);
+
+    const now = new Date();
+    savingLink.textContent = `Download save file (from ${now.toLocaleString()})`;
+    savingLink.href = lastSavingBlobURL;
+    savingLink.download = formatSaveFileNameWithDate(now);
+}
+
+function onSavingFileInputChange(e, input) {
+    const file = e.target.files[0];
+    if(!file) {
+        return;
+    }
+
+    try {
+        FS.unlink('/out.kk-save');
+    } catch {}
+
+    const reader = new FileReader();
+    reader.onload = (loadE) => {
+        const arrayBuffer = loadE.target.result;
+        const u8array = new Uint8Array(arrayBuffer);
+        FS.writeFile('/in.kk-save', u8array);
+    };
+
+    reader.readAsArrayBuffer(file);
+}
+
+function loadGame() {
+    if(!FS.analyzePath('/in.kk-save').exists) {
+        window.alert("A save file wasn't loaded yet.");
+        return;
+    }
+
+    const result = Module.ccall('minizip_load_store', 'string', ['boolean', 'string'], [false, '/in.kk-save']);
+    if(result[0] === 'k') {
+        const sep = result.indexOf(';');
+
+        const width = parseInt(result.substring(1, sep));
+        const height = parseInt(result.substring(sep + 1));
+
+        if(width == null || height == null) {
+            console.error('Cannot parse width and height from save loading result');
+            return;
+        }
+
+        initGame({x: width, y: height}, false);
+    } else if(result[0] === 'e') {
+        window.alert(`Error when loading your game! ${result.substring(1)}`);
+    } else {
+        console.error('Invalid load error message', result);
+    }
+}
