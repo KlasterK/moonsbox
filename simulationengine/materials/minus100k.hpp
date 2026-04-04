f18c3d66cd5280d071d0bd365c13eb7a11c74c32
KlasterK <kadakinvi2@gmail.com>
2026-04-04 14:46:29 +0900

    fix: -100 K material making temperature negative

--- simulationengine/materials/minus100k.hpp
+++ simulationengine/materials/minus100k.hpp
@@ -8,7 +8,7 @@ class Minus100K : public MaterialController
 public:
     inline void init_point(GameMap &map, size_t x, size_t y) override
     {
-        map.temps(x, y) -= 100.f;
+        map.temps(x, y) = std::max(0.001f, map.temps(x, y) - 100.f);
     }
 };
 
